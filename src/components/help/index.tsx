72a7d71319fc14507fc0b4323fafefdc6256cc00
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-06-11 20:39:53 +0400

    back button added

--- src/components/help/index.tsx
+++ src/components/help/index.tsx
@@ -1,9 +1,11 @@
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
 import { FaBalanceScaleLeft } from "react-icons/fa";
 import { IoCallSharp, IoPeopleSharp } from "react-icons/io5";
 import "./style.scss";
 import { useState } from "react";
 import { selectHelp, selectedHelp } from "../../redux/slice";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 type Props = {
   publicDecide: never;
   phoneDecide: never;
@@ -17,6 +19,8 @@ function Help({ publicDecide, phoneDecide, eliminate }: Props) {
   const [helpType, sethelpType] = useState("");
   const dispatch = useDispatch();
   const prevHelp = useSelector((state: State) => state.quiz.prevHelp);
+
+  const navigate = useNavigate();
   const helps = [
     {
       value: "half",
@@ -35,37 +39,48 @@ function Help({ publicDecide, phoneDecide, eliminate }: Props) {
     },
   ];
   return (
-    <div className="Help">
-      <div className="helps">
-        {helps.map((help) => {
-          return (
-            <button
-              disabled={prevHelp.find((elem) => elem == help.value)}
-              className="help"
-              key={help.value}
-              onMouseEnter={() => {
-                sethelpType(help.info);
-              }}
-              onMouseLeave={() => {
-                sethelpType("");
-              }}
-              onClick={() => {
-                help.value == "people"
-                  ? publicDecide()
-                  : help.value == "call"
-                  ? phoneDecide()
-                  : help.value == "half"
-                  ? eliminate()
-                  : null;
-                //  : console.log(help);
-                dispatch(selectHelp(help.value));
-                dispatch(selectedHelp(help.value));
-              }}
-            >
-              {help.icon}
-            </button>
-          );
-        })}
+    <div className="helpCont">
+      <div className="Help">
+        <div
+          className="back"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          <MdOutlineKeyboardArrowLeft size={25} color="#31304D" />
+        </div>
+        <div className="helps">
+          {helps.map((help) => {
+            return (
+              <button
+                disabled={prevHelp.find((elem) => elem == help.value)}
+                className="help"
+                key={help.value}
+                onMouseEnter={() => {
+                  sethelpType(help.info);
+                }}
+                onMouseLeave={() => {
+                  sethelpType("");
+                }}
+                onClick={() => {
+                  help.value == "people"
+                    ? publicDecide()
+                    : help.value == "call"
+                    ? phoneDecide()
+                    : help.value == "half"
+                    ? eliminate()
+                    : null;
+                  //  : console.log(help);
+                  dispatch(selectHelp(help.value));
+                  dispatch(selectedHelp(help.value));
+                }}
+              >
+                {help.icon}
+              </button>
+            );
+          })}
+        </div>
+        <div></div>
       </div>
       <div className="info">{helpType}</div>
     </div>
